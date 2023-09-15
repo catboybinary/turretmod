5e5a7cd3bd4c8dc5d220e3f383132a6385077f7e
binary <catboybinary@disroot.org>
2023-09-16 01:28:41 +0300

    now it works properly, the only thing left is to make it compatible with all of the projectiles

--- src/main/java/me/binary/turretmod/block/entity/TurretEntity.java
+++ src/main/java/me/binary/turretmod/block/entity/TurretEntity.java
@@ -1,16 +1,20 @@
 package me.binary.turretmod.block.entity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Vec3i;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.Containers;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
@@ -19,9 +23,13 @@ import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+import java.util.Random;
+
 public class TurretEntity extends BlockEntity {
 
-    private static int timer;
+    private static final Random random = new Random();
+    private int timer = 0;
     private final ItemStackHandler itemHandler = new ItemStackHandler(1) {
         @Override
         protected void onContentsChanged(int slot) {
@@ -82,11 +90,21 @@ public class TurretEntity extends BlockEntity {
 
     public static void tick(Level level, BlockPos blockPos, BlockState blockState, TurretEntity e) {
         if (e.itemHandler.getStackInSlot(0).getCount() > 0 && e.itemHandler.getStackInSlot(0).is(Items.ARROW)) {
-            Entity a = EntityType.ARROW.create(level);
-            a.setPos(Vec3.atCenterOf(blockPos.above()));
-            a.setDeltaMovement(Math.random()-0.5d,1d,Math.random()-0.5d);
-            level.addFreshEntity(a);
-            System.out.println(a.position());
+            if (level.isClientSide) return;
+            List<LivingEntity> entities = level.getEntitiesOfClass(LivingEntity.class,new AABB(blockPos.subtract(new Vec3i(5,5,5)),blockPos.subtract(new Vec3i(-5,-5,-5))));
+            if (entities.isEmpty()) return;
+            e.timer = (e.timer + 1) % 20;
+            if (e.timer>0) return;
+
+            LivingEntity target = entities.get(random.nextInt(entities.size()));
+            Projectile projectile = EntityType.ARROW.create(level);
+            projectile.setPos(Vec3.atCenterOf(blockPos.above()));
+            Vec3 targetPosition = target.position().add(0,target.getEyeHeight()/2,0);
+            Vec3 blockPosition = Vec3.atCenterOf(blockPos.above());
+            projectile.setDeltaMovement(targetPosition.subtract(blockPosition).normalize()
+                    .add(0,targetPosition.distanceTo(blockPosition)/20,0));
+            level.addFreshEntity(projectile);
+
             ItemStack n = e.itemHandler.getStackInSlot(0);
             n.setCount(n.getCount()-1);
             e.itemHandler.setStackInSlot(0,n);
